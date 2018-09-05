00ad90559e2f4481d78794e271c1596442a2c494
sehta7@gmail.com <sehta7@gmail.com>
2018-09-05 16:23:19 +0200

    add method to add user to file

--- src/registration/system/User.java
+++ src/registration/system/User.java
@@ -1,9 +1,17 @@
 package registration.system;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class User {
 
 	private String name;
 	private int age;
+	
+	public User() {
+		
+	}
 	
 	public User(String name, int age) {
 		super();
@@ -16,5 +24,19 @@ public class User {
 		return "User [name=" + name + ", age=" + age + "]";
 	}
 	
-	
+	public void addUser() {
+		try {
+			System.out.println("Give your name: ");
+			Scanner sc = new Scanner(System.in);
+			name = sc.nextLine();
+			System.out.println("Give our age: ");
+			age = sc.nextInt();
+			PrintWriter pw = new PrintWriter("registration.txt");
+			pw.print("name: " + name + ", age: " + age);
+			pw.close();
+			
+		} catch (IOException e) {
+			System.out.println("File error!");
+		}
+	}
 }
