657ed944af05dc802c6db50e7d1341533de5880b
sehta7@gmail.com <sehta7@gmail.com>
2019-01-03 20:53:11 +0100

    move menu to new class

--- src/registration/system/Application.java
+++ src/registration/system/Application.java
@@ -1,42 +1,19 @@
 package registration.system;
 
-import java.util.Scanner;
+import java.io.IOException;
+
+import registration.system.menu.Menu;
 
 public class Application {
 
 	public static void main(String[] args) {
-		
-		showMenu();
 
-	}
-	
-	public static void showMenu() {
-		User user = new User();
-		
-		System.out.println("What you want to do?");
-		System.out.println("1. Add user");
-		System.out.println("2. Delete user");
-		System.out.println("3. Exit");
-		
-		Scanner sc = new Scanner(System.in);
-		int choice = sc.nextInt();
-		
-		switch (choice) {
-		case 1:
-			user.addUser();
-			showMenu();
-			break;
-		case 2:
-			user.deleteUser();
-			showMenu();
-			break;
-		case 3:
-			System.exit(0);
-		default:
-			System.out.println("Choose another option.");
-			showMenu();
-			break;
+		try {
+			Menu.showMenu();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
 	}
 
 }
